13f0b6bfa1883deee9fba9b885eb6c0e85eed6c1
MOHAMEDMESKINE1 <meskinemohameddev@gmail.com>
2025-10-27 14:27:19 +0100

    fix route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: '',
-      component: () => import('../views/Index.vue'),
+      component: () => import('../views/index.vue')
     },
   ],
 })
